88433ca2b46146a2fa8db836df05f6d26835cd76
polovykh <polovykh@mail.ru>
2023-12-16 23:34:49 +0700

    Step 3

---
+++ gendiff/__init__.py
@@ -0,0 +1,3 @@
+from gendiff.src.find_differences import generate_diff
+
+__all__ = "generate_diff"
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from gendiff.src.find_differences import cli_gendiff
+
+
+def main():
+    cli_gendiff()
+
+
+if __name__ == "__main__":
+    main()
